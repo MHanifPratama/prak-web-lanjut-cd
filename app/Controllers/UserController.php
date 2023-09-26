afd73550bb57a5ecd74db7bd5c3befefb3a0bbeb
M Hanif Pratama <mhanifp27@gmail.com>
2023-09-26 07:22:51 +0700

    Next

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
+use App\Models\KelasModel;
 
 class UserController extends BaseController
 {
@@ -10,27 +12,78 @@ class UserController extends BaseController
     {
         //
     }
-    public function profile($nama ="", $npm =""): string
+    public function profile($nama ="", $npm ="",$kelas=""): string
     {
         $data=[
             'nama'=>$nama,
-            'npm'=>$npm
+            'npm'=>$npm,
+            'kelas'=>$kelas
         ];
         return view('profile',$data);
     }
     public function create()
     {
-        return view('create_user');
+        $kelas = [
+            [
+                'id'=>1,
+                'nama_kelas'=>'A'
+            ],
+            [
+                'id'=>2,
+                'nama_kelas'=>'B'
+            ],
+            [
+                'id'=>3,
+                'nama_kelas'=>'C'
+            ],
+            [
+                'id'=>4,
+                'nama_kelas'=>'D'
+            ],
+        ];
+        $data=[
+            'kelas'=>$kelas
+        ];
+        return view('create_user',$data);
     }
+
     public function store()
     {
+        
+        $userModel = new UserModel();
+        $kelasModel = new KelasModel();
         $nama = $this->request->getPost('nama');
         $npm = $this->request->getPost('npm');
+        $kelas = $this->request->getPost('kelas');
+        dd($this->request->getPost('kelas')) ;
+
+        // Validate
+        if (!$this->validate([
+            'nama' => 'required',
+            'npm'=>[
+                'rules'=>'required|numeric|is_unique[user.npm]',
+                'errors'=>[
+                    'required'=>'NPM harus diisi',
+                    'numeric'=>'NPM harus berupa angka',
+                    'is_unique'=>'NPM sudah terdaftar'
+                ]
+            ],
+            'kelas' => 'required'
+        ])) {
+            return redirect()->to('/user/create')->withInput()->with('validation', $this->validator);
+        }
         $data=[
             'nama'=>$nama,
-            'npm'=>$npm
+            'npm'=>$npm,
+            'id_kelas'=>$kelas
         ];
-        // dd($data);
-        return view('profile',$data);
+
+        $userModel->saveUser($data);
+        $data_new=[
+            'nama'=>$nama,
+            'npm'=>$npm,
+            'id_kelas'=>$kelasModel->find($kelas)['nama_kelas']
+        ];
+        return view('profile',$data_new);
     }
 }
